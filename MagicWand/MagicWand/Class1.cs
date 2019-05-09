3bfdd9ae05f08e242940eb827c0cb9b8c0d11848
Echo-Gambino <nkb012pomm@gmail.com>
2019-05-09 16:31:06 -0500

    Got MagicWand Basic Functionality to work

--- MagicWand/MagicWand/Class1.cs
+++ MagicWand/MagicWand/Class1.cs
@@ -2,20 +2,81 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Autodesk.Revit.UI;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI.Selection;
 
 namespace MagicWand
 {
-    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Transaction(TransactionMode.Manual)]
     public class Class1 : IExternalCommand
     {
-        public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit, ref string message, ElementSet elements)
+        public Result Execute(ExternalCommandData revit, ref string message, ElementSet elements)
         {
-            TaskDialog.Show("Revit", "Hello World");
-            return Autodesk.Revit.UI.Result.Succeeded;
+            // Get application and document objects
+            UIApplication uiapp = revit.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            IList<Reference> pickedrefs = null;
+
+            ICollection<ElementId> ids = null;
+
+            Selection sel = null;
+
+            try
+            {
+
+                // Have the user select elements
+                sel = uidoc.Selection;
+                pickedrefs = sel.PickObjects(ObjectType.Element, "Select Elements");
+                // Use LINQ to get ElemendId of all References in pickedrefs
+                ids = (from Reference r in pickedrefs select r.ElementId).ToList();
+
+                // Print out items that have been selected
+                using (Transaction trans = new Transaction(doc))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    trans.Start("Transaction");
+                    if (pickedrefs != null && pickedrefs.Count > 0)
+                    {
+                        foreach (ElementId eid in ids)
+                        {
+                            Element elem = doc.GetElement(eid);
+                            sb.Append("\n" + elem.Name);
+                        }
+                        TaskDialog.Show("Debug", "Items Selected : " + sb.ToString());
+                    }
+                    trans.Commit();
+                }
+
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException ex)
+            {
+                TaskDialog.Show("Debug", "ex is " + ex.Message);
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+
+    public class CommonTools
+    {
+        public void HighlightElements()
+        {
+            return;
         }
     }
 }
